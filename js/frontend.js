e9313aa314a08b030f1351b82b4c73fcbb65debe
James Holdsworth <james@theholdsworths.org.uk>
2012-11-23 20:13:34 +0000

    Removed a load of SVN crap and fixed formatting in JS/CSS files

--- js/frontend.js
+++ js/frontend.js
@@ -3,26 +3,26 @@ var numberOfMethods = 0; // the number of method input boxes (i.e. methods in cu
 
 /* ONLOAD function */
 $(document).ready(function() {
-	loadMethodsForStage($('#methodRank').val());
+    loadMethodsForStage($('#methodRank').val());
 
-	$('#methodRank').change(function () {
-		removeMethodBoxes();
-		loadMethodsForStage($('#methodRank').val());
-	});
+    $('#methodRank').change(function () {
+        removeMethodBoxes();
+        loadMethodsForStage($('#methodRank').val());
+    });
 
-	$("textarea").elastic();
+    $("textarea").elastic();
 
-	$("#composition").trigger('keyup');
-	
-	checkLiveProve();
+    $("#composition").trigger('keyup');
 
-	$("#tabs").tabs();
+    checkLiveProve();
 
-	$(".notation").blur(function() {
-		var not = $(this).val();
-		not = not.replace(/x/g, '-');
-		$(this).val(not);
-	});
+    $("#tabs").tabs();
+
+    $(".notation").blur(function() {
+        var not = $(this).val();
+        not = not.replace(/x/g, '-');
+        $(this).val(not);
+    });
 });
 
 /* EVENT HANDLERS */
@@ -30,7 +30,7 @@ $(document).ready(function() {
 $("#searchMethod").click(displayMethodLibraryPage);
 
 $('#moreMethods').click(function(){
-	insertMethodBox("", "");
+    insertMethodBox("", "");
 });
 
 $("#insertMethod").click(insertMethod);
@@ -42,164 +42,164 @@ $("#liveProve").click(checkLiveProve);
 $("#prove").click(prove);
 
 $('.callLocation').change(function() {
-	displayWarning('Half-lead bobs are not implemented yet');
+    displayWarning('Half-lead bobs are not implemented yet');
 });
 
 $("#generateShorthand").click(function() {
-	var firstMethod = $('#methodList').children(':first-child');
-	var mid = $(firstMethod[0]).attr('id');
-	if(!mid) {
-		displayWarning('No methods defined.');
-	} else {
-		generateShorthand(mid.substr(6), [$('#symbol0').val(), $('#callNtn0').val()], [$('#symbol1').val(), $('#callNtn1').val()]);
-//	displayWarning('This feature is still experimental. Check the output in the composition box looks correct', 3000);
-		prove();
-	}
+    var firstMethod = $('#methodList').children(':first-child');
+    var mid = $(firstMethod[0]).attr('id');
+    if(!mid) {
+        displayWarning('No methods defined.');
+    } else {
+        generateShorthand(mid.substr(6), [$('#symbol0').val(), $('#callNtn0').val()], [$('#symbol1').val(), $('#callNtn1').val()]);
+        //	displayWarning('This feature is still experimental. Check the output in the composition box looks correct', 3000);
+        prove();
+    }
 });
 
 // add more call fields
 $('#btnAddMoreCalls').click(function() {
-	var children=$('#calls table tr').size() - 1;
-	$('#calls table').append('<tr><td><input type="text" size="1" maxlength="1" name="symbol'+children+'" id="symbol'+children+'" value="" /></td><td><input type="text" size="3" name="callNtn'+children+'" id="callNtn'+children+'" value="" /></td><td><select class="callLocation" name="callLocation'+children+'" id="callLocation'+children+'"><option value="le">Lead End</option><option value="hl">Half Lead</option></select></td></tr>');
+    var children=$('#calls table tr').size() - 1;
+    $('#calls table').append('<tr><td><input type="text" size="1" maxlength="1" name="symbol'+children+'" id="symbol'+children+'" value="" /></td><td><input type="text" size="3" name="callNtn'+children+'" id="callNtn'+children+'" value="" /></td><td><select class="callLocation" name="callLocation'+children+'" id="callLocation'+children+'"><option value="le">Lead End</option><option value="hl">Half Lead</option></select></td></tr>');
 });
 
 /* FUNCTIONS */
 // function to add a new input box into the method list section
 function insertMethodBox(code, pn) {
-	$('#methodList').append('<div id="method' + numberOfMethods + '"><input type="text" id="shortcut' + numberOfMethods + '" maxlength="1" size="1" value="'+code+'" /><input type="text" class="notation" id="notation' + numberOfMethods + '" value="'+pn+'" size="35" /><input type="button" value="&dash;" class="removeMethod" onclick="removeParent(this);" /></div>');
-	numberOfMethods++;
+    $('#methodList').append('<div id="method' + numberOfMethods + '"><input type="text" id="shortcut' + numberOfMethods + '" maxlength="1" size="1" value="'+code+'" /><input type="text" class="notation" id="notation' + numberOfMethods + '" value="'+pn+'" size="35" /><input type="button" value="&dash;" class="removeMethod" onclick="removeParent(this);" /></div>');
+    numberOfMethods++;
 }
 
 function removeMethodBoxes() {
-	$('#methodList').html("");
+    $('#methodList').html("");
 }
 
 // insert the selected method into the methods list
 function insertMethod() {
-	var method_name = $("#methodSelect option:selected").text();
-	var method_symbol = $("#methodSymbol").val();
-	if(checkMethodLetterIsUnique(method_name.charAt(0), method_name, method_symbol)) {
-		if(method_symbol == "") {
-			insertMethodBox(method_name.charAt(0), $("#methodSelect").val());
-		} else {
-			insertMethodBox(method_symbol, $("#methodSelect").val());
-		}
-	}
-	closePopup();
+    var method_name = $("#methodSelect option:selected").text();
+    var method_symbol = $("#methodSymbol").val();
+    if(checkMethodLetterIsUnique(method_name.charAt(0), method_name, method_symbol)) {
+        if(method_symbol == "") {
+            insertMethodBox(method_name.charAt(0), $("#methodSelect").val());
+        } else {
+            insertMethodBox(method_symbol, $("#methodSelect").val());
+        }
+    }
+    closePopup();
 }
 
 // removes an element's parent from the dom
 function removeParent(me) {
-	$(me).parent().remove();
+    $(me).parent().remove();
 }
 
 // either enable live proving or disable it and give a prove button
 function checkLiveProve() {
-	if($('#liveProve').attr('checked')) {
-		$('#composition').keyup(function() {
-			prove();
-		});
-		$('#prove').hide();
-	} else {
-		$('#composition').unbind('keyup');
-		$('#prove').show();
-	}
+    if($('#liveProve').attr('checked')) {
+        $('#composition').keyup(function() {
+            prove();
+        });
+        $('#prove').hide();
+    } else {
+        $('#composition').unbind('keyup');
+        $('#prove').show();
+    }
 }
 
 // checks if a letter is unique - gives an error if not.
 function checkMethodLetterIsUnique(letter, method_name, method_symbol) {
-	// trawl through the DOM to find matching methods
-	var is_unique = true;
-	for(i = 0; i < numberOfMethods; i++) {
-		if(method_symbol == "") {
-			if($('#shortcut'+i).val() === letter) {
-            	is_unique = false;
-			}
-		} else {
-			if($('#shortcut'+i).val() === method_symbol) {
-				is_unique = false;
-			}
-		}
-		if(is_unique === false) {
-			displayWarning("A method with shortcut <strong>" + letter + "</strong> already exists. " + method_name + " has not been added.");
-			return is_unique;
-			break;
-		}
-	} 
-	return is_unique;
+    // trawl through the DOM to find matching methods
+    var is_unique = true;
+    for(i = 0; i < numberOfMethods; i++) {
+        if(method_symbol == "") {
+            if($('#shortcut'+i).val() === letter) {
+                is_unique = false;
+            }
+        } else {
+            if($('#shortcut'+i).val() === method_symbol) {
+                is_unique = false;
+            }
+        }
+        if(is_unique === false) {
+            displayWarning("A method with shortcut <strong>" + letter + "</strong> already exists. " + method_name + " has not been added.");
+            return is_unique;
+            break;
+        }
+    }
+    return is_unique;
 }
 
 function loadMethodsForStage(stage) {
-	switch (stage) {
-		case '6':
-			insertMethodBox("C", "-3-4-2-3-4-5,2");
-			break;
+    switch (stage) {
+        case '6':
+            insertMethodBox("C", "-3-4-2-3-4-5,2");
+            break;
 
-		case '8':
-			insertMethodBox("Y", "-3-4-5-6-2-3-4-7,2");
-			insertMethodBox("S", "-36-4-5-36-4-5-36-7,2");
-			insertMethodBox("B", "-5-4.5-5.36.4-4.5-4-1,1");
-			break;
+        case '8':
+            insertMethodBox("Y", "-3-4-5-6-2-3-4-7,2");
+            insertMethodBox("S", "-36-4-5-36-4-5-36-7,2");
+            insertMethodBox("B", "-5-4.5-5.36.4-4.5-4-1,1");
+            break;
 
-		case '10':
-			insertMethodBox("B", "-5-4.5-5.36.4-7.58.6-6.7-6-1,1");
-			insertMethodBox("C", "-3-4-25-36-47-58-6-7-8-9,2");
-			insertMethodBox("Y", "-3-4-5-6-27-38-4-5-6-9,2");
-			break;
+        case '10':
+            insertMethodBox("B", "-5-4.5-5.36.4-7.58.6-6.7-6-1,1");
+            insertMethodBox("C", "-3-4-25-36-47-58-6-7-8-9,2");
+            insertMethodBox("Y", "-3-4-5-6-27-38-4-5-6-9,2");
+            break;
 
-		case '12':
-			insertMethodBox("C", "-3-4-25-36-47-58-69-70-8-9-0-E,2");
-			insertMethodBox("N", "-3-4-5-6-7-8-9-0-8-9-70-E,2");
-			insertMethodBox("Y", "-3-4-5-6-27-38-49-50-6-7-8-E,2");
-			break;
+        case '12':
+            insertMethodBox("C", "-3-4-25-36-47-58-69-70-8-9-0-E,2");
+            insertMethodBox("N", "-3-4-5-6-7-8-9-0-8-9-70-E,2");
+            insertMethodBox("Y", "-3-4-5-6-27-38-49-50-6-7-8-E,2");
+            break;
 
-	}
+    }
 }
 
 function prove() {
-	try {
-		res = do_prove();
-	} catch (e) {
-		res = "Error: " + e;
-	}
-	$('#results').html(res[0]);
-	$('#music').html(res[1]);
-	$('#courseEnds').html(res[2]);
+    try {
+        res = do_prove();
+    } catch (e) {
+        res = "Error: " + e;
+    }
+    $('#results').html(res[0]);
+    $('#music').html(res[1]);
+    $('#courseEnds').html(res[2]);
 }
 
 function displayMethodLibraryPage() {
-	var method_file = $('#class').val() + $('#methodRank').val();
-	loadMethods(method_file);
+    var method_file = $('#class').val() + $('#methodRank').val();
+    loadMethods(method_file);
 }
 
 function loadMethods(file) {
-	$.get('lib/'+file, function(data) {
-		var methods = "<label for=\"methodSymbol\">Shortcut</label><input type=\"text\" size=\"2\" maxlength=\"1\" name=\"methodSymbol\" id=\"methodSymbol\" />";
-		methods += "<select name=\"method\" id=\"methodSelect\">";
-		var m = data.split("\n");
-		for(i=1; i<m.length; i++) {
-			var n = m[i].split(" ");
-			methods += "<option value='"+n[1]+" "+n[2]+"'>" + n[0] + "</option>"
-		}
-		methods += "</select><input type=\"button\" id=\"insertMethod\" value=\"Insert Method\" onclick=\"insertMethod();\" />";
+    $.get('lib/'+file, function(data) {
+        var methods = "<label for=\"methodSymbol\">Shortcut</label><input type=\"text\" size=\"2\" maxlength=\"1\" name=\"methodSymbol\" id=\"methodSymbol\" />";
+        methods += "<select name=\"method\" id=\"methodSelect\">";
+        var m = data.split("\n");
+        for(i=1; i<m.length; i++) {
+            var n = m[i].split(" ");
+            methods += "<option value='"+n[1]+" "+n[2]+"'>" + n[0] + "</option>"
+        }
+        methods += "</select><input type=\"button\" id=\"insertMethod\" value=\"Insert Method\" onclick=\"insertMethod();\" />";
 
-		$('#popup').html(methods + "<span style=\"float:right\"><a href=\"javascript:closePopup();\">close</a></span>");
-		$('#popup').fadeIn('slow');
-	}, "text");
+        $('#popup').html(methods + "<span style=\"float:right\"><a href=\"javascript:closePopup();\">close</a></span>");
+        $('#popup').fadeIn('slow');
+    }, "text");
 }
 
 function displayWarning(warning, timeout) {
-	if(timeout === undefined) {
-		timeout = 2000;
-	}
-	$('#warning div').html(warning);
-	$('#warning').fadeIn('slow').delay(timeout).fadeOut('slow');
+    if(timeout === undefined) {
+        timeout = 2000;
+    }
+    $('#warning div').html(warning);
+    $('#warning').fadeIn('slow').delay(timeout).fadeOut('slow');
 }
 
 function hideWarning() {
-	$('#warning').fadeOut('slow');
+    $('#warning').fadeOut('slow');
 }
 
 function closePopup() {
-	$('#popup').fadeOut('slow');
+    $('#popup').fadeOut('slow');
 }
--- js/pealprover.js
+++ js/pealprover.js
@@ -1,249 +1,249 @@
 function get_composition() {
-	var methods = {};
+    var methods = {};
 
-	var comptext = $('#composition').val();
-	var comp = Composition();
-	var done_call = true;
-	var last_method;
+    var comptext = $('#composition').val();
+    var comp = Composition();
+    var done_call = true;
+    var last_method;
 
-	// TODO: warn about failures
-	$('#methodList').each(function() {
-		for(i=0; i < $(this).children().length; i++) {
-			var vm = validate_method($(this).children()[i].id.substr(6));
-			methods[vm[0]] = vm[1];
-		}
-	});
+    // TODO: warn about failures
+    $('#methodList').each(function() {
+        for(i=0; i < $(this).children().length; i++) {
+            var vm = validate_method($(this).children()[i].id.substr(6));
+            methods[vm[0]] = vm[1];
+        }
+    });
 
-	var txtCalls = new Object();
-	for (n = 0; n < $('#calls table tr').size()-1; n++) {
-		var symbol = $("#symbol"+n).val();
-		var not = $("#callNtn"+n).val();
-		txtCalls[symbol] = parse_bell_list(comp.rank, 0, not);
-	}
+    var txtCalls = new Object();
+    for (n = 0; n < $('#calls table tr').size()-1; n++) {
+        var symbol = $("#symbol"+n).val();
+        var not = $("#callNtn"+n).val();
+        txtCalls[symbol] = parse_bell_list(comp.rank, 0, not);
+    }
 
-	var first = true;
-	var is_ce = false;
-	var last_call = -1;
+    var first = true;
+    var is_ce = false;
+    var last_call = -1;
 
-	for (i = 0; i < comptext.length; i++) {
-		c = comptext.charAt(i);
-		
-		if (c in methods) {
-			if (!done_call) {
-				comp.append_lead(last_method, -1);
-			}
-			last_method = methods[c];
-			done_call = false;
-		} else if (c in txtCalls) {
-			comp.append_lead(last_method, txtCalls[c].mask);
-			done_call = true;
-		} else if (c === '\n') {
-			is_ce = true;
-		}
-	}
-	if (!done_call) {
-		comp.append_lead(last_method, -1);
-	}
+    for (i = 0; i < comptext.length; i++) {
+        c = comptext.charAt(i);
 
-	return [comp, txtCalls];
+        if (c in methods) {
+            if (!done_call) {
+                comp.append_lead(last_method, -1);
+            }
+            last_method = methods[c];
+            done_call = false;
+        } else if (c in txtCalls) {
+            comp.append_lead(last_method, txtCalls[c].mask);
+            done_call = true;
+        } else if (c === '\n') {
+            is_ce = true;
+        }
+    }
+    if (!done_call) {
+        comp.append_lead(last_method, -1);
+    }
+
+    return [comp, txtCalls];
 }
 
 function validate_method(id) {
-	var shortcut;
-	var rows;
-	var rank;
-	var method;
-	var notation;
-   
-	shortcut = $('#shortcut' + id).val();
-	if (shortcut == "") {
-		return false;
-	}
-	notation = $('#notation' + id).val();
-	if (notation == "") {
-		return false;
-	}
-	rank = parseInt($('#methodRank').val(), 10);
-	if (rank <= 0 || rank > MAX_RANK) {
-		return false;
-	}
-	rows = parse_method(rank, notation);
-	method = Method(rank, shortcut, rows);
-	//methods[shortcut] = method;
-	return [shortcut, method];
-	//return true;
+    var shortcut;
+    var rows;
+    var rank;
+    var method;
+    var notation;
+
+    shortcut = $('#shortcut' + id).val();
+    if (shortcut == "") {
+        return false;
+    }
+    notation = $('#notation' + id).val();
+    if (notation == "") {
+        return false;
+    }
+    rank = parseInt($('#methodRank').val(), 10);
+    if (rank <= 0 || rank > MAX_RANK) {
+        return false;
+    }
+    rows = parse_method(rank, notation);
+    method = Method(rank, shortcut, rows);
+    //methods[shortcut] = method;
+    return [shortcut, method];
+//return true;
 }
 
 function do_prove() {
-	var p = Prover();
-	var changes = 0;
-	var rounds = 0;
-	var music = MusicBox();
+    var p = Prover();
+    var changes = 0;
+    var rounds = 0;
+    var music = MusicBox();
 
-	function test_row(c, le) {
-		var r;
+    function test_row(c, le) {
+        var r;
 
-		changes++;
-		if (rounds !== 0) {
-			return;
-		}
-		music.match_row(c);
-		r = p.check_row(c);
+        changes++;
+        if (rounds !== 0) {
+            return;
+        }
+        music.match_row(c);
+        r = p.check_row(c);
 
-		if (r < 0) {
-			rounds = -changes;
-		} else if (r == 1) {
-			rounds = changes;
-		}
-	}
+        if (r < 0) {
+            rounds = -changes;
+        } else if (r == 1) {
+            rounds = changes;
+        }
+    }
 
-	var gc = get_composition();
-	var comp = gc[0];
-	var txtCalls = gc[1];
-	var musicOutput;
-	
-	// add 4-bell run patterns whatever the stage (rank)
-	for(i=0; i < comp.rank; i++) {
-		// fill up the rest of the music array with the correct number of -1's
-		var spareBells = new Array();
-		var runs = new Array();
-		for(j=0; j < (comp.rank-4); j++) {
-			spareBells[j] = -1;
-		}
-        
-		// music for all numbers of bells
-        
-		// forward run off the front (ie 1234.... etc)
-		runs = runs.concat([i, i+1, i+2, i+3], spareBells);
-		music.add_pattern(runs);
-		runs = new Array();
-    	
-		// backward run off the front (ie 4321....)
-		runs = runs.concat([i+3, i+2, i+1, i], spareBells);
-		music.add_pattern(runs);
-		runs = new Array();
-    	
-		// forward run at the back (ie ....1234)
-		runs = runs.concat(spareBells, [i, i+1, i+2, i+3]);
-		music.add_pattern(runs);
-		runs = new Array();
-    	
-		// backward run at the back (ie ....4321)
-		runs = runs.concat(spareBells, [i+3, i+2, i+1, i]);
-		music.add_pattern(runs);
-        
-		spareBells, runs = null; // clean up - not sure this is necessary
-	}
-	/* other good 8-bell specific music - hard coded for enhanced user "experience" :-) */
-	if(comp.rank === 8) {
-		music.add_pattern([-1,-1,-1,-1,6, 4, 5, 7]); // ****7568
-		music.add_pattern([-1,-1,-1,-1,5, 4, 6, 7]); // ****6578
-		music.add_pattern([-1,-1,-1,-1,1, 3, 5, 7]); // ****2468
-		music.add_pattern([0,2,4,6,1, 3, 5, 7]); // 13572468
-		music.add_pattern([6,4,2,0,1, 3, 5, 7]); // 75312468
-		music.add_pattern([0,4,1,5,2,6,3,7]); // 15263748
-		music.add_pattern([0,1,6,4,2,3,5,7]); // 12753468
-	}
-	
-	// add user specific music patterns
-	//TODO: put this in a separate method and don't hard code #userMusicList you numpty
-	var userInputPatterns = $('#userMusicList').val();
-	if(userInputPatterns.length != 0) {
-		var userPatterns = readUserMusicPatterns(userInputPatterns);
-		for(i = 0; i < userPatterns.length; i++) {
-			// ignore empty values (ie line feeds)
-			if(userPatterns[i].length != 0) {
-				music.add_pattern(userPatterns[i]);
-			}
-		}
-	}
-	
-	music.setup(comp.rank);
-	comp.run(test_row);
-	
-	// display course ends
-	var comptext = $('#composition').val();
+    var gc = get_composition();
+    var comp = gc[0];
+    var txtCalls = gc[1];
+    var musicOutput;
 
-	// now remove the calls from the comp text (look up from call list)
-	// split the composition up at the carriage returns.
-	for(i in txtCalls) {
-		var regex = new RegExp(i, 'g');
-		comptext = comptext.replace(regex, '');
-	}
+    // add 4-bell run patterns whatever the stage (rank)
+    for(i=0; i < comp.rank; i++) {
+        // fill up the rest of the music array with the correct number of -1's
+        var spareBells = new Array();
+        var runs = new Array();
+        for(j=0; j < (comp.rank-4); j++) {
+            spareBells[j] = -1;
+        }
 
-	comptext = comptext.split("\n");
+        // music for all numbers of bells
 
-	var leads = [];
-	for(i=0; i<comp.leadends.length; i++) {
-		var lead = "";
-		for(j=0; j<comp.leadends[i].length; j++) {
-			lead += bell_name(comp.leadends[i][j]);
-		}
-		leads[i] = lead;
-	}
+        // forward run off the front (ie 1234.... etc)
+        runs = runs.concat([i, i+1, i+2, i+3], spareBells);
+        music.add_pattern(runs);
+        runs = new Array();
 
-	var courses = "";
-	var leadNo = 0;
-	for(c = 0; c < comptext.length; c++) {
-		if(comptext.length > 0) {
-			leadNo = comptext[c].length + leadNo;
-			courses += leads[leadNo-1] + "\n";
-		}
-	}
+        // backward run off the front (ie 4321....)
+        runs = runs.concat([i+3, i+2, i+1, i], spareBells);
+        music.add_pattern(runs);
+        runs = new Array();
 
-	if (rounds > 0) {
-		if (rounds != changes) {
-			res = "<span class=\"true\">Touch is true: " + rounds + " changes (" + (changes - rounds) + " before last course end)</span>";
-		} else {
-			res = "<span class=\"true\">Touch is true: " + rounds + " changes</span>";
-		}
-	} else if (rounds < 0) {
-		res = "<span class=\"false\">Touch is false: false after " + (-rounds) + " changes</span>";
-	} else {
-		res = "<span class=\"incomplete\">Incomplete touch (" + changes + " changes)</span>";
-	}
+        // forward run at the back (ie ....1234)
+        runs = runs.concat(spareBells, [i, i+1, i+2, i+3]);
+        music.add_pattern(runs);
+        runs = new Array();
 
-	musicOutput = "<pre>";
-	for (i = 0; i < music.counts.length; i++) {
-		var pattern = "";
-		// add run count to music output array only if those runs exist
-		if(music.counts[i] != 0 ) {
-			for(j=0; j < music.patterns[i].length; j++) {
-				pattern += bell_name(music.patterns[i][j]);
-			}
-			musicOutput += "<strong>" + music.counts[i] + "</strong>\t" + pattern + "\n";
-		}
-	}
-	musicOutput += "</pre>";
-    
-	return [res, musicOutput, courses];
+        // backward run at the back (ie ....4321)
+        runs = runs.concat(spareBells, [i+3, i+2, i+1, i]);
+        music.add_pattern(runs);
+
+        spareBells, runs = null; // clean up - not sure this is necessary
+    }
+    /* other good 8-bell specific music - hard coded for enhanced user "experience" :-) */
+    if(comp.rank === 8) {
+        music.add_pattern([-1,-1,-1,-1,6, 4, 5, 7]); // ****7568
+        music.add_pattern([-1,-1,-1,-1,5, 4, 6, 7]); // ****6578
+        music.add_pattern([-1,-1,-1,-1,1, 3, 5, 7]); // ****2468
+        music.add_pattern([0,2,4,6,1, 3, 5, 7]); // 13572468
+        music.add_pattern([6,4,2,0,1, 3, 5, 7]); // 75312468
+        music.add_pattern([0,4,1,5,2,6,3,7]); // 15263748
+        music.add_pattern([0,1,6,4,2,3,5,7]); // 12753468
+    }
+
+    // add user specific music patterns
+    //TODO: put this in a separate method and don't hard code #userMusicList you numpty
+    var userInputPatterns = $('#userMusicList').val();
+    if(userInputPatterns.length != 0) {
+        var userPatterns = readUserMusicPatterns(userInputPatterns);
+        for(i = 0; i < userPatterns.length; i++) {
+            // ignore empty values (ie line feeds)
+            if(userPatterns[i].length != 0) {
+                music.add_pattern(userPatterns[i]);
+            }
+        }
+    }
+
+    music.setup(comp.rank);
+    comp.run(test_row);
+
+    // display course ends
+    var comptext = $('#composition').val();
+
+    // now remove the calls from the comp text (look up from call list)
+    // split the composition up at the carriage returns.
+    for(i in txtCalls) {
+        var regex = new RegExp(i, 'g');
+        comptext = comptext.replace(regex, '');
+    }
+
+    comptext = comptext.split("\n");
+
+    var leads = [];
+    for(i=0; i<comp.leadends.length; i++) {
+        var lead = "";
+        for(j=0; j<comp.leadends[i].length; j++) {
+            lead += bell_name(comp.leadends[i][j]);
+        }
+        leads[i] = lead;
+    }
+
+    var courses = "";
+    var leadNo = 0;
+    for(c = 0; c < comptext.length; c++) {
+        if(comptext.length > 0) {
+            leadNo = comptext[c].length + leadNo;
+            courses += leads[leadNo-1] + "\n";
+        }
+    }
+
+    if (rounds > 0) {
+        if (rounds != changes) {
+            res = "<span class=\"true\">Touch is true: " + rounds + " changes (" + (changes - rounds) + " before last course end)</span>";
+        } else {
+            res = "<span class=\"true\">Touch is true: " + rounds + " changes</span>";
+        }
+    } else if (rounds < 0) {
+        res = "<span class=\"false\">Touch is false: false after " + (-rounds) + " changes</span>";
+    } else {
+        res = "<span class=\"incomplete\">Incomplete touch (" + changes + " changes)</span>";
+    }
+
+    musicOutput = "<pre>";
+    for (i = 0; i < music.counts.length; i++) {
+        var pattern = "";
+        // add run count to music output array only if those runs exist
+        if(music.counts[i] != 0 ) {
+            for(j=0; j < music.patterns[i].length; j++) {
+                pattern += bell_name(music.patterns[i][j]);
+            }
+            musicOutput += "<strong>" + music.counts[i] + "</strong>\t" + pattern + "\n";
+        }
+    }
+    musicOutput += "</pre>";
+
+    return [res, musicOutput, courses];
 }
 
 // parses a music pattern and converts it into jsprove format
 function readUserMusicPatterns(patternList) {
-	patternList = patternList.split("\n");
-	
-	for(j=0; j < patternList.length; j++) {	
-		var pattern = patternList[j].split("");
-		for(i=0; i < pattern.length; i++) {
-			// do the opposite of what we do in do_prove() - convert bell numbers into array items
-			pattern[i] = bell_index(pattern[i]);
-		}
-		patternList[j] = pattern;
-	}
-	return patternList;
+    patternList = patternList.split("\n");
+
+    for(j=0; j < patternList.length; j++) {
+        var pattern = patternList[j].split("");
+        for(i=0; i < pattern.length; i++) {
+            // do the opposite of what we do in do_prove() - convert bell numbers into array items
+            pattern[i] = bell_index(pattern[i]);
+        }
+        patternList[j] = pattern;
+    }
+    return patternList;
 }
 
 function generateShorthand(methodID, bob, single) {
-	var vm = validate_method(methodID);
-	var s = Shorthand($('#shorthand').val(), vm[1]);
+    var vm = validate_method(methodID);
+    var s = Shorthand($('#shorthand').val(), vm[1]);
 
-	try {
-		s.run(nothing, bob, single);
-	} catch (er) {
-		displayWarning(er);
-	}
-	$('#composition').val(s.compText);
+    try {
+        s.run(nothing, bob, single);
+    } catch (er) {
+        displayWarning(er);
+    }
+    $('#composition').val(s.compText);
 
-	function nothing() {}
+    function nothing() {}
 }

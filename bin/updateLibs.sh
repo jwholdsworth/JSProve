6eb4939413aed386e632f051241385b130ad2ac5
James Holdsworth <james.holdsworth@orange.com>
2012-12-07 14:26:55 +0000

    Update the method libraries. Should these really be stored in git?

--- bin/updateLibs.sh
+++ bin/updateLibs.sh
@@ -5,4 +5,5 @@ cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd /tmp/
 wget http://www.ringsoft.co.uk/change-ringers/ringing-programs/microsiril/method-libraries/mslibs.zip
 unzip mslibs.zip -d lib/
+dos2unix lib/*
 cp lib/* $cwd/../lib/

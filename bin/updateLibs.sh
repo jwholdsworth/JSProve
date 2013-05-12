1e877664214bf4a594eed9e87c2e78e4e320b48e
James Holdsworth <james@theholdsworths.org.uk>
2013-05-12 10:26:03 +0100

    Updated updateLibs.sh script to improve error handling; updated libs.

--- bin/updateLibs.sh
+++ bin/updateLibs.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+## Helper function ##
+function logError {
+    echo -e "\033[1;31m[ERROR] $1\033[0m";
+    exit 1;
+}
+
+# get PWD
 cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd /tmp/
-wget http://www.ringsoft.co.uk/change-ringers/ringing-programs/microsiril/method-libraries/mslibs.zip
-unzip mslibs.zip -d lib/
-dos2unix lib/*
+# download the latest micro-siril libraries
+cd /tmp/ && wget http://www.ringsoft.co.uk/change-ringers/ringing-programs/microsiril/method-libraries/mslibs.zip > /dev/null 2>&1 || logError 'Could not download the MicroSiril libraries'
+
+# unzip them
+unzip mslibs.zip -d lib/ > /dev/null 2>&1 || logError 'Something went wrong unzipping the MicroSiril libraries'
+
+# remove ridiculous windows characters
+dos2unix lib/* > /dev/null 2>&1 || logError 'There was a problem converting the libraries to Unix format'
+
+# copy the cleansed libraries to the lib directory
 cp lib/* $cwd/../lib/
+
+echo -e "\033[1;32m[SUCCESS] The MicroSiril libraries have been updated. You will now need to commit them, and deploy them to the server.\033[0m";

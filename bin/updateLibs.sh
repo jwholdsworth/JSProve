e8ccd596639fab9061ac61343a4b62d907f1e171
James Holdsworth <james.holdsworth@infinityworks.com>
2015-05-06 15:59:10 +0100

    Updated the method libraries (well overdue!).

--- bin/updateLibs.sh
+++ bin/updateLibs.sh
@@ -13,7 +13,7 @@ cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd /tmp/ && wget http://www.ringsoft.co.uk/change-ringers/ringing-programs/microsiril/method-libraries/mslibs.zip > /dev/null 2>&1 || logError 'Could not download the MicroSiril libraries'
 
 # unzip them
-unzip mslibs.zip -d lib/ > /dev/null 2>&1 || logError 'Something went wrong unzipping the MicroSiril libraries'
+cd $cwd && unzip /tmp/mslibs.zip -d lib/ > /dev/null 2>&1 || logError 'Something went wrong unzipping the MicroSiril libraries'
 
 # remove ridiculous windows characters
 dos2unix lib/* > /dev/null 2>&1 || logError 'There was a problem converting the libraries to Unix format'

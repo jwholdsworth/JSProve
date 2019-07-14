a55a4412f08478f7eefa995f41763d4c4beb455b
James Holdsworth <jwholdsworth@gmail.com>
2019-07-14 13:19:06 +0100

    Adding test framework.

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "extends": "eslint:recommended"
+};

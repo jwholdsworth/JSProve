0189f6764528da2b95d85b968f65783a37d7d997
James Holdsworth <jwholdsworth@gmail.com>
2020-05-26 09:21:46 +0100

    Added spacing and more UI improvements.

--- js/frontend.js
+++ js/frontend.js
@@ -2,14 +2,15 @@
 /** *****************************************************************************
  * DECLARE FRONT-END VARIABLES
  *******************************************************************************/
-let numberOfMethods = 0; // the number of method input boxes (i.e. methods in current composition)
+// the number of method input boxes (i.e. methods in current composition)
+let numberOfMethods = 0;
 let numberOfCalls = 0;
 let stage = $('#methodRank').val();
 
 /** *****************************************************************************
  * ONLOAD FUNCTION
  *******************************************************************************/
-$(document).ready(function() {
+$(document).ready(function () {
   setup();
   // Enable autosizing of all text areas
   autosize($('textarea'));
@@ -28,11 +29,11 @@ $('body').tooltip();
  * EVENT HANDLERS
  *******************************************************************************/
 // Reset the user interface when stage changes
-$('#methodRank').change(function() {
+$('#methodRank').change(function () {
   setup();
 });
 
-$('#collectionChoice').change(function() {
+$('#collectionChoice').change(function () {
   insertCollection(stage, $('option:selected', this).val());
 });
 
@@ -40,7 +41,7 @@ $('#collectionChoice').change(function() {
 $('#searchMethod').click(displayMethodLibraryPage);
 
 // Load the 'Insert new Method' menu
-$('#moreMethods').click(function() {
+$('#moreMethods').click(function () {
   insertMethodBox('', '');
 });
 
@@ -48,17 +49,17 @@ $('#moreMethods').click(function() {
 $('#insertMethod').click(insertMethod);
 
 // Display a warning when you try to use half lead calls (not implemented yet)
-$('.callLocation').change(function() {
+$('.callLocation').change(function () {
   displayMessage('Half-lead calls are not implemented yet');
 });
 
 // add event handler to composition box - on change, prove the composition
-$('#composition').keyup(function() {
+$('#composition').keyup(function () {
   prove();
 });
 
 // generate the composition from the shorthand
-$('#generateShorthand').click(function() {
+$('#generateShorthand').click(function () {
   // select the first method from the method list to be used for the shorthand
   const firstMethod = $('#methodList').children(':first-child');
   const mid = $(firstMethod[0]).attr('id');
@@ -79,8 +80,8 @@ $('#generateShorthand').click(function() {
 });
 
 // add more call fields
-$('#btnAddMoreCalls').click(function() {
-  insertCallBox('', '')
+$('#btnAddMoreCalls').click(function () {
+  insertCallBox('', '');
 });
 
 /** *****************************************************************************
@@ -114,7 +115,7 @@ function insertCallBox(symbol, notation) {
             <input type="button" value="&dash;" class="removeCall btn btn-danger" onclick="removeElement('call${numberOfCalls}');" />
         </div>
     </div>
-  `
+  `;
   $('#js-calls-form').append(callRow);
   numberOfCalls++;
 }
@@ -126,18 +127,18 @@ function insertCallBox(symbol, notation) {
  */
 function insertMethodBox(code, pn) {
   const methodRow = `
-    <div id="method${numberOfMethods}" class="form-row js-method">
-      <div class="col-1">
-        <input type="text" class="js-shortcut form-control" id="shortcut${numberOfMethods}" maxlength="1" value="${code}" />
+    <div id="method${numberOfMethods}" class="form-row js-method mb-1">
+      <div class="col-2">
+        <input type="text" class="js-shortcut form-control" id="shortcut${numberOfMethods}" maxlength="1" value="${code}" placeholder="Symbol" />
       </div>
       <div class="col">
-        <input type="text" class="js-notation form-control" id="notation${numberOfMethods}" value="${pn}"  />
+        <input type="text" class="js-notation form-control" id="notation${numberOfMethods}" value="${pn}" placeholder="Notation" />
       </div>
       <div class="col-1">
         <input type="button" value="&dash;" class="removeMethod btn btn-danger" onclick="removeElement('method${numberOfMethods}');" />
       </div>
     </div>
-  `
+  `;
   $('#methodList').append(methodRow);
   numberOfMethods++;
 }
@@ -160,9 +161,10 @@ function insertMethod() {
 
 /**
  * Removes a method from the Method List
+ * @param {string} domId
  */
 function removeElement(domId) {
-  document.getElementById(domId).remove()
+  document.getElementById(domId).remove();
 }
 
 /**
@@ -192,8 +194,8 @@ function checkMethodLetterIsUnique(letter, methodName, methodSymbol) {
 
 /**
  * Returns a list of method collections for this particular stage
- * @param string stage
- * @return array of Collection objects
+ * @param {string} stage
+ * @return {array} of Collection objects
  */
 function getCollectionsForStage(stage) {
   for (let i = 0; i < collections.collections.length; i++) {
@@ -207,6 +209,7 @@ function getCollectionsForStage(stage) {
 
 /**
  * Loads the collections associated with this particular stage into a drop-down
+ * @param {string} stage
  */
 function loadCollectionsForStage(stage) {
   stageCollections = getCollectionsForStage(stage);
@@ -235,15 +238,15 @@ function insertCollection(stage, key) {
 
 function loadCalls() {
   calls.forEach((call) => {
-    insertCallBox(call.symbol, call.notation)
-  })
+    insertCallBox(call.symbol, call.notation);
+  });
 }
 
 /**
  * Applies some default music for this stage
  */
 function loadMusicForStage(stage) {
-  $.get('music/' + stage, function(data) {
+  $.get('music/' + stage, function (data) {
     $('#userMusicList').val(data);
   }, 'text');
   autosize.update($('#userMusicList'));
@@ -289,7 +292,7 @@ function displayMethodLibraryPage() {
  * Display the Load Method from File box
  */
 function loadMethods(file) {
-  $.get('lib/' + file, function(data) {
+  $.get('lib/' + file, function (data) {
     let methods = '';
     methods += '<select name="method" id="methodSelect" data-live-search="true" class="form-control">';
     const m = data.split('\n');
